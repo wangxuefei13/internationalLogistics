2269f49576bd22a547a7b6853cf10c9ab0802dab
llh <3243535338@qq.com>
2020-12-30 15:52:53 +0800

    llh-001

---
+++ src/main/java/com/crm/entity/Account.java
@@ -0,0 +1,22 @@
+package com.crm.entity;
+
+public class Account {
+    private Integer account_id;
+    private String account_name;
+
+    public Integer getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(Integer account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getAccount_name() {
+        return account_name;
+    }
+
+    public void setAccount_name(String account_name) {
+        this.account_name = account_name;
+    }
+}
